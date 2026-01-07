2713ce1d9c76c7b3a01de2a3b79874ba2f1d934e
giovannagrigo <giovanna1grigo@gmail.com>
2026-01-07 11:35:05 -0300

    Dinamização de dados por id na tela do perfil do contato

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from "../../componentes/container/container.component";
 import { Contato } from '../../componentes/contato/contato';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ContatoService } from '../../services/contato.service';
 
 @Component({
   selector: 'app-perfil-contato',
@@ -15,14 +16,28 @@ import { RouterLink } from '@angular/router';
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
 })
-export class PerfilContatoComponent {
+export class PerfilContatoComponent implements OnInit {
 
-    contato: Contato = {
-      id: 0,
-      nome: 'dev',
-      telefone: '123123123',
-      email: 'dev@email.com',
-      aniversario: '21/10/2006',
-      redes: 'dev.com'
+  contato: Contato = {
+    id: 0,
+    nome: '',
+    telefone: '',
+    email: '',
+    aniversario: '',
+    redes: ''
+  }
+
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private contatoService: ContatoService
+  ) {}
+
+  ngOnInit() {
+    const id =this.activatedroute.snapshot.paramMap.get('id')
+    if(id) {
+      this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+        this.contato = contato
+      })
     }
+  }
 }
