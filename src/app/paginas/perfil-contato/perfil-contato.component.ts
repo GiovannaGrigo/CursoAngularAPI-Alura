ecd13696b423b89bfb30a083d8cefab4055947fd
giovannagrigo <giovanna1grigo@gmail.com>
2026-01-07 11:21:29 -0300

    Alteração no packae.json do backend para garantir que o id vai ser salvo como Number

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from "../../componentes/container/container.component";
 import { Contato } from '../../componentes/contato/contato';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
   imports: [
     CommonModule,
-    ContainerComponent
+    ContainerComponent,
+    RouterLink
   ],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
